bf146665ecdb009026b54ddfabd00b849409cab7
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-09-13 12:57:45 -0500

    Display Name Program

--- src/outputexamples/DisplayName.java
+++ src/outputexamples/DisplayName.java
@@ -1,5 +1,10 @@
 package outputexamples;
 
 public class DisplayName {
-System.out.println("Cody Edmond");
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Cody Edmond");
+	}
+
 }
