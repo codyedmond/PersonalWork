f4d131eaf1d0b5200a3329aa2e061e9051440b4a
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-09-13 13:41:42 -0500

    StudentSchedule

---
+++ src/outputexamples/StudentSchedule.java
@@ -0,0 +1,16 @@
+package outputexamples;
+
+public class StudentSchedule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Cody Edmond");
+System.out.println("1st Block Study Hall");
+System.out.println("2nd Block Algebra 2");
+System.out.println("2nd Block Alternate Avid 11")
+System.out.println("3rd Block Crime Society Law");
+System.out.println("4th Block AP Computer Science");
+
+	}
+
+}
