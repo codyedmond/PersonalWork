4a5e19ed55f367b35c77d01bf32870c1c7de140e
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-09-13 13:10:48 -0500

    FirstClassMade

---
+++ src/outputexamples/Firstclassmade.java
@@ -0,0 +1,12 @@
+package outputexamples;
+
+public class Firstclassmade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Cody");
+System.out.println("Edmond");
+	}
+	
+
+}
