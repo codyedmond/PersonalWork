a9fa525a8d634496c7569fd60ce3c5356613b218
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-09-13 14:02:27 -0500

    FixedLikesAndDislikes

--- src/outputexamples/LikesAndDislikes.java
+++ src/outputexamples/LikesAndDislikes.java
@@ -4,12 +4,11 @@ public class LikesAndDislikes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Likes				Dislikes");
-System.out.println("Football			Reading");
-System.out.println("Video Games			Writing Essays");
-System.out.println("Weight Lifting 		Sprint Work");
-System.out.println("Traveling           Brussel Sprouts");
-System.out.println("Sleeping			Doing Chores");
-
-
+		System.out.println("Likes				Dislikes");
+		System.out.println("Football			Reading");
+		System.out.println("Video Games			Writing Essays");
+		System.out.println("Weight Lifting 		Sprint Work");
+		System.out.println("Traveling           Brussel Sprouts");
+		System.out.println("Sleeping			Doing Chores");
+	}
 }
