889af68bf2b1570e1d6dd17ac6ccc8090cabde4e
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-09-19 15:07:26 -0500

    MonkeyInput

---
+++ src/inputExamples/Monkey.java
@@ -0,0 +1,15 @@
+package inputExamples;
+
+public class Monkey {
+import java.util.Scanner;
+public class inputExample{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double money = -999;
+Scanner input = new Scanner(System.in);
+System.out.println("What is the adult hieght of a monkey?");
+money=input.nextDouble();
+System.out.println("An adult monkey is" + Inches tall ");
+	}
+
+}
