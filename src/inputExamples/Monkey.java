3e1389405a356bf194f5871407d98580b65ca981
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-09-20 13:11:26 -0500

    DidYouVoteInput

--- src/inputExamples/Monkey.java
+++ src/inputExamples/Monkey.java
@@ -1,15 +1,17 @@
 package inputExamples;
-
-public class Monkey {
 import java.util.Scanner;
+public class Monkey {
+
 public class inputExample{
+	
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-double money = -999;
+int monkey = -999;
 Scanner input = new Scanner(System.in);
-System.out.println("What is the adult hieght of a monkey?");
-money=input.nextDouble();
-System.out.println("An adult monkey is" + Inches tall ");
+System.out.println("How tall is an adult monkey?");
+monkey = input.nextInt();
+System.out.println("Adult monkey is " + "monkey + hieght");
 	}
 
 }
--- src/outputexamples/StudentSchedule.java
+++ src/outputexamples/StudentSchedule.java
@@ -7,7 +7,7 @@ public class StudentSchedule {
 System.out.println("Cody Edmond");
 System.out.println("1st Block Study Hall");
 System.out.println("2nd Block Algebra 2");
-System.out.println("2nd Block Alternate Avid 11")
+System.out.println("2nd Block Alternate Avid 11");
 System.out.println("3rd Block Crime Society Law");
 System.out.println("4th Block AP Computer Science");
 
