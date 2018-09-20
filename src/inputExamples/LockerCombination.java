b66933496f2b895c29b26e3c6c8c3ccb72277883
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-09-20 14:06:06 -0500

    LockerCombinationInput

---
+++ src/inputExamples/LockerCombination.java
@@ -0,0 +1,22 @@
+package inputExamples;
+import java.util.Scanner;
+public class LockerCombination {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int first = 19;
+int second = 33;
+int third = 11;
+Scanner input = new Scanner(System.in);
+System.out.println("What is the first locker number");
+first = input.nextInt();
+System.out.println("What is the second locker number");
+second = input.nextInt();
+System.out.println("What is the third locker number");
+third= input.nextInt();
+System.out.println("Your first locker number is" + first);
+System.out.println("Your first locker number is" + second);
+System.out.println("Your first locker number is" + third);
+	}
+
+}
