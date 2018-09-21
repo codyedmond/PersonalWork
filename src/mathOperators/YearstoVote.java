70cd71d2c1ea913828306c4de17bfa79ecb5fa7c
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-09-21 14:33:32 -0500

    YearstoVoteMathOpp

---
+++ src/mathOperators/YearstoVote.java
@@ -0,0 +1,15 @@
+package mathOperators;
+import java.util.Scanner;
+public class YearstoVote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age = 16;
+Scanner input = new Scanner(System.in);
+System.out.println("How old are you");
+age= input.nextInt();
+age = age - 14;
+System.out.println("There are " + age + "years until you can vote");
+	}
+
+}
