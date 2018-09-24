8f61837118cef4647c9304466be6c2b084883171
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-09-24 15:08:01 -0500

    SpeedingTicketMathOpp

---
+++ src/mathOperators/SpeedingTicket.java
@@ -0,0 +1,21 @@
+package mathOperators;
+import java.util.Scanner;
+public class SpeedingTicket {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int speedlimit = -999;
+int driverspeed = -999;
+int cost = -999;
+int initial  = 55;
+int extra = 40;
+Scanner input= new Scanner(System.in);
+System.out.println("What is the speed limit?");
+speedlimit=input.nextInt();
+System.out.println("What was your speed?");
+driverspeed=input.nextInt();
+System.out.println("Your fine is" + cost);
+cost= initial + (2 * 40);
+	}
+
+}
