a20eddafd32c1a87d5b254567796fce259b2cb7c
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-09-26 14:40:42 -0500

    SocialSecurityNumberMathOpp

--- src/mathOperators/LetsPaint.java
+++ src/mathOperators/LetsPaint.java
@@ -4,7 +4,8 @@ public class LetsPaint {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+double length1= -999.9;
+double width1 = -999.9;
 	}
 
 }
--- src/mathOperators/SpeedingTicket.java
+++ src/mathOperators/SpeedingTicket.java
@@ -9,15 +9,16 @@ int driverspeed = -999;
 int cost = -999;
 int initial  = 55;
 int extra = 40;
-int milesover=-999;
+int milesover= 1;
 Scanner input= new Scanner(System.in);
 System.out.println("What is the speed limit?");
 speedlimit=input.nextInt();
 System.out.println("What was your speed?");
 driverspeed=input.nextInt();
 System.out.println("How many miles over the speedlimit did you go?");
+milesover=input.nextInt();
 System.out.println("Your fine is" + cost);
-cost= initial + extra * (milesover -1);
+cost= initial + extra * (milesover - 1);
 	}
 
 }
