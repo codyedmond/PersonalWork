ce6d214d5c6bf0be6f009f8bb3d2040c9a7c0e51
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-10-02 14:39:31 -0500

    KidTestClass

---
+++ src/objectorientedprogram/TestKid.java
@@ -0,0 +1,13 @@
+package objectorientedprogram;
+
+public class TestKid {
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+Kid Boy= new Kid();
+Boy.sayMama();
+Boy.sayFood();
+Boy.sayStink();
+	}
+
+}
