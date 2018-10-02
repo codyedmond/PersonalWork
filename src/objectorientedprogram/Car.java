8e7efaf6ab62e30a8b3be9449e5773db0006222e
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-10-02 14:25:36 -0500

    CarTestClass

--- src/objectorientedprogram/Car.java
+++ src/objectorientedprogram/Car.java
@@ -1,6 +1,15 @@
 package objectorientedprogram;
 
 public class Car {
+	public Car()
 {
+}
+public void sayOdometer(){
+System.out.println("25,000");
+}
+public void sayFlat(){
+System.out.println("There is a flat tire for the car");}
+public void sayThirsty(){
+System.out.println("The car is out of gas");
 }
 }
---
+++ src/objectorientedprogram/TestCar.java
@@ -0,0 +1,15 @@
+package objectorientedprogram;
+
+public class TestCar {
+	
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+	Car Toyota= new Car();
+		Toyota.sayOdometer();
+Toyota.sayFlat();
+Toyota.sayThirsty();
+	}
+
+}
