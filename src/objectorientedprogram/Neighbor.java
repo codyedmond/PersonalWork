a39a782fea969dd96699be485bde2da9ce29ab85
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-10-02 14:50:45 -0500

    NeighborTestClass

---
+++ src/objectorientedprogram/Neighbor.java
@@ -0,0 +1,14 @@
+package objectorientedprogram;
+
+public class Neighbor {
+	public Neighbor() 
+	public void sayJoke(){
+	System.out.println("Why did Adele cross the road,To say hello from the other side! ");}
+	public void sayMoney(){
+	System.out.println("Your friend wants your money");}
+	public void sayConversation(){
+	System.out.println("How are you?, I am good, hbu?, pretty good myself");}
+	
+	{
+}
+}
---
+++ src/objectorientedprogram/NeighborTest.java
@@ -0,0 +1,13 @@
+package objectorientedprogram;
+
+public class NeighborTest {
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+Neighbor Fred= new Neighbor();
+Fred.sayJoke();
+Fred.sayMoney();
+Fred.sayConversation();
+	}
+
+}
