978d73061fe0caf8e87cb6b22751c293a47c63af
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-10-05 14:56:22 -0500

    NumberClassOOP

--- src/objectorientedprogram/Numbers.java
+++ src/objectorientedprogram/Numbers.java
@@ -1,5 +1,26 @@
 package objectorientedprogram;
 
 public class Numbers {
-
+public Numbers()
+{
+}
+public void sayNumber(double num) {
+	System.out.println("Your number is:" + num);
+}
+public void sayNumberPlus2(double num) {
+	num= num=+2;
+	System.out.println("Your number plus 2 is:" + num);}
+public void saySum(double first, double second){
+	System.out.println("answer=" + (first + second))}
+	public double returnSquare(double value){
+	double answer= value*value;
+	return answer;
+	
+		
+	}
+	public double returnArea(double length, double width){
+	double area= length * width;
+	{
+		return second;
+}
 }
--- src/objectorientedprogram/NumbersTestClass.java
+++ src/objectorientedprogram/NumbersTestClass.java
@@ -4,6 +4,16 @@ public class NumbersTestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Numbers bob= new Numbers();
+bob.sayNumber(7.5);
+Numbers stacy= new Numbers();
+stacy.sayNumberPlus2(7.5);
+Numbers jennifer= new Numbers();
+jennifer.saySum( 2,2);
+Numbers don = new Numbers();
+don.returnSquare(7.0);
+double answer =bob.returnArea(5.0,4.0);
+System.out.println(answer);
 
 	}
 
