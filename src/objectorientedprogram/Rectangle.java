019425eb8158e6f8180aa0d7e0a1070cb937a56f
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-10-10 14:54:11 -0500

    RectanglePlusTestClass

--- src/objectorientedprogram/Rectangle.java
+++ src/objectorientedprogram/Rectangle.java
@@ -1,9 +1,37 @@
 package objectorientedprogram;
 
 public class Rectangle {
-private double length;
-private double width;
-public Rectangle()
-{
-}
+	private double length;
+	private double width;
+	private double perimeter;
+	private double area;
+
+	public Rectangle(double length, double width) {
+		length = 6;
+		width = 5;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public double getPerimeter() {
+		return (length + length + width + width);
+	}
+
+	public double getArea() {
+		return (length * width);
+	}
+
+	public void changeLength(double newLength) {
+		length = newLength;
+	}
+
+	public void changeWidth(double newWidth) {
+		width = newWidth;
+	}
 }
