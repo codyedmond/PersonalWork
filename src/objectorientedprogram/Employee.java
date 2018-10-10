0530315c5d3f5ad4d27a957f007bedae57cbdf43
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-10-10 14:03:28 -0500

    EmployeeOop

--- src/objectorientedprogram/Employee.java
+++ src/objectorientedprogram/Employee.java
@@ -1,5 +1,31 @@
 package objectorientedprogram;
 
 public class Employee {
-
+private double wage;
+private double experience;
+public Employee()
+{
 }
+{
+wage = 7.75;
+experience = 2;
+}
+public double getWage(){
+	return wage;
+}
+public double setWage(double newWage){
+	wage=newWage;
+}
+public double getExperience(){
+	return experience;
+}
+public double setExperience(double newExperience)
+{
+	experience=newExperience;
+			
+}
+public void giveRaise(){
+	wage=wage*1.10;
+}
+}}
+
