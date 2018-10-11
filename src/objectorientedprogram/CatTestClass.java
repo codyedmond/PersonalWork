f5d86fe3687bf851f78c2dcc8dfba5e5a1821f82
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-10-11 13:34:18 -0500

    CatClass

---
+++ src/objectorientedprogram/CatTestClass.java
@@ -0,0 +1,13 @@
+package objectorientedprogram;
+
+public class CatTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Cat bob = new Cat();
+System.out.println(bob.birthday());
+System.out.println(bob.sleepMore());
+System.out.println(bob.sleepLess());
+	}
+
+}
---
+++ src/objectorientedprogram/WorkerTestClass.java
@@ -0,0 +1,10 @@
+package objectorientedprogram;
+
+public class WorkerTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
