0cea2f09e5dcef1ea5829d32c7ea38d123890c45
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-10-12 13:33:46 -0500

    RectangleFinish

--- src/objectorientedprogram/Cat.java
+++ src/objectorientedprogram/Cat.java
@@ -1,5 +1,21 @@
 package objectorientedprogram;
 
 public class Cat {
+private int age;
+private int sleephours;
 
+public Cat(){
+ age=16;
+ sleephours=7;
+}
+
+public int birthday(){
+	return age+1;
+}
+public int sleepMore(){
+return sleephours +1;
+}
+public int sleepLess(){
+return sleephours-1;
+}
 }
--- src/objectorientedprogram/RightTriangleTestClass.java
+++ src/objectorientedprogram/RightTriangleTestClass.java
@@ -4,7 +4,10 @@ public class RightTriangleTestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+RightTriangle bob = new RightTriangle();
+bob.hypot();
+bob.perimeter();
+bob.area();
 	}
 
 }
