ac11ef5ca993fc59875207355926489d37a4e7b5
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-10-22 15:04:15 -0500

    ChangedTempDecisionStructures

--- src/decisions/Temperature.java
+++ src/decisions/Temperature.java
@@ -16,19 +16,23 @@ int nothingtemp=input.nextInt();
 if(swimtemp>83)
 {
 System.out.println("It is good weather for swimming");
-
+}
+else
 if(tennistemp>74 && tennistemp<83)
 {
 System.out.println("It is a good weather for tennis");
-
+}
+else
 if(golftemp>35 &&  golftemp<=74)
 {
 System.out.println("It is good weather for golf");
-
+}
+else
 if(snowshoetemp>-10 && snowshoetemp<=35)
 {
 System.out.println("It is good weather for snowshoeing");
-
+}
+else
 if(nothingtemp <=-10)
 {
 System.out.println("It is a good day to do nothing");
