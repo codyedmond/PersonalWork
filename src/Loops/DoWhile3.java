7411028d9090cf8e0b1bac6f0bfbc18f55a2233b
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-10-24 15:07:13 -0500

    Finish3DoWhileLoops

--- src/Loops/DoWhile3.java
+++ src/Loops/DoWhile3.java
@@ -1,9 +1,18 @@
 package Loops;
-
+import java.util.Scanner;
 public class DoWhile3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Scanner input= new Scanner(System.in);
+System.out.println("Enter two numbers");
+int numbers=input.nextInt();
+do{
+System.out.println("Enter a number(9999 to quit):");
+int numbers=input.nextInt();
+}while(number !=9999;)
+	System.out.println(largest);
+
 
 	}
 
