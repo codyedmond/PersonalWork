28643d2f0e045a9613a2d9deb8d4c1dda4996192
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-10-29 14:44:06 -0500

    NestedLoops

---
+++ src/Loops/RowForLoops.java
@@ -0,0 +1,38 @@
+package Loops;
+
+public class RowForLoops {
+
+	public static void main(String[] args) {
+	}
+	}
+		// TODO Auto-generated method stub
+/*		int number = 0;
+
+		for (number = 1; number <= 5; number = number++) {
+			System.out.print(number);
+			number += 1;
+		}
+		for (number = 2; number <= 10; number = number++) {
+			System.out.print(number);
+			number += 2;
+		}
+		for (number = 3; number <= 15; number = number++) {
+			System.out.print(number);
+			number += 3;
+		}
+		for (number = 4; number <= 20; number = number++) {
+			System.out.print(number);
+			number += 4;
+		}
+	}
+}
+*/
+
+		int number = 0;
+
+	for (number=1;  number>=5; number=num++) 
+	{
+		if(number!=3)
+		System.out.println(number);
+	
+}}}
