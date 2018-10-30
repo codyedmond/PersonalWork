c597a7800157fcac07023a5fe02dd391e70a1f40
Cody Edmond <20edmonc@elmbrookstudents.org>
2018-10-30 14:38:51 -0500

    ChangesToNestedLoops

--- src/Loops/DivisorsNestedForLoops.java
+++ src/Loops/DivisorsNestedForLoops.java
@@ -1,9 +1,22 @@
 package Loops;
-
+import java.util.Scanner;
 public class DivisorsNestedForLoops {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+System.out.println("Enter a number");
+int number =input.nextInt();
+for(number=1; number<=1000; number=number++ )
+{
+	if(number=)
+}
+{
+	
+}
+
+
+
 
 	}
 
--- src/Loops/RowForLoops.java
+++ src/Loops/RowForLoops.java
@@ -4,7 +4,7 @@ public class RowForLoops {
 
 	public static void main(String[] args) {
 	}
-	}
+	
 		// TODO Auto-generated method stub
 /*		int number = 0;
 
